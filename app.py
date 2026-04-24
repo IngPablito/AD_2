fdd1f0a5acc588cfcfc8183f3436dd523cb1e3e4
Juan Pablo Vayas <pablisvatru12@gmail.com>
2026-04-24 00:29:10 +0000

    correccion

--- app.py
+++ app.py
@@ -102,6 +102,7 @@ def listar_productos():
                 "id": row[0],
                 "nombre": row[1],
                 "precio": float(row[2]) if row[2] is not None else None,
+                
             })
 
         return jsonify({
